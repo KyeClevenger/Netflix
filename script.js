88f0baea3906692931279031725550a68836183a
Kye Clevenger <KyeClevenger@gmail.com>
2024-02-01 22:18:32 -0700

    five

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', function () {
+    const isExpanded = this.getAttribute('aria-expanded') === 'true';
+    this.setAttribute('aria-expanded', !isExpanded);
+    mobileMenu.hidden = isExpanded;
+    });
+});
